49275c67060640fc2e34a00de81e72d533b8fd6a
Basavaraj-sh <basavaraj.sh@gmail.com>
2022-12-08 18:47:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_compute_network" "vpc" {
   name                    = var.network_name
   project                 = var.project_id
-  auto_create_subnetworks = var.auto_create_subnetworks
+  auto_create_subnetworks = false
  }
