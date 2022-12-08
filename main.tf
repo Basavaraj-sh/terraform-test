ad3ff287d01418cf32c61957237d5ad034303f8e
Basavaraj-sh <basavaraj.sh@gmail.com>
2022-12-08 19:09:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,10 @@ resource "google_compute_network" "vpc" {
   project                 = var.project_id
   auto_create_subnetworks = false
  }
+
+resource "null_resource" "list-cluster" {
+
+  provisioner "local-exec" {
+    command = "gcloud compute networks list --project=${var.project_id}"
+  }
+}
