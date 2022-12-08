db9c5d2eba9817822ec3f29380a4405664bb44e3
Basavaraj-sh <basavaraj.sh@gmail.com>
2022-12-08 17:36:28 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,5 @@
+resource "google_compute_network" "vpc" {
+  name                    = var.network_name
+  project                 = var.project_id
+  auto_create_subnetworks = var.auto_create_subnetworks
+ }
